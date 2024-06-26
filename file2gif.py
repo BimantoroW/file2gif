3434b1dc79524e139ce28679ae9a686ff9700e1b
bimantorow <bimantoro.widyadana@ui.ac.id>
2024-06-26 22:46:27 +0700

    cli improvements

--- file2gif.py
+++ file2gif.py
@@ -2,9 +2,16 @@ import sys
 from encoder import Encoder
 from decoder import Decoder
 
+DEFAULT_SIZE = (1000, 1000)
+
+def help():
+    print(f'Usage: py {sys.argv[0]} [-d] file [widthXheight]')
+
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        raise ValueError('bro')
+    if ((len(sys.argv) < 2 or len(sys.argv) > 3)
+        or (sys.argv[1] == '--help' or sys.argv[1] == '-h')):
+        help()
+        exit(0)
     
     if sys.argv[1] == '-d':
         path = sys.argv[2]
@@ -12,8 +19,11 @@ if __name__ == '__main__':
         dec.decode(path)
     else:
         path = sys.argv[1]
-        size = sys.argv[2]
-        size = size.split('x')
-        size = (int(size[0]), int(size[1]))
+        if len(sys.argv) == 3:
+            size = sys.argv[2]
+            size = size.split('X') 
+            size = (int(size[0]), int(size[1]))
+        else:
+            size = DEFAULT_SIZE
         enc = Encoder(path, size)
         enc.encode()
